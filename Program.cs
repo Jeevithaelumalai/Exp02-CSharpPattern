022dca5c471cf87b80cee92ff4c40da79dd9d290
jeevithaelumlai <jeevithajeevitha542@gmail.com>
2024-03-03 21:34:00 +0530

    add

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
-﻿
-using System;
-public class Pattern
+﻿using System;
+
+class Program
 {
-    public static void Main()
+    static void Main()
     {
-        // type the code here
+        int rows;
+        rows = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Pascal's triangle"); 
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                int val = 1;
+                for (int k = 1; k <= rows - i; k++)
+                {
+                    val *= (i - j + k) / k;
+                }
+                Console.Write(val + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
